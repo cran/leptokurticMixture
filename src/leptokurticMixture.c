405074013ddc29e844c222f19a2f398a80c163f6
Ryan Browne <rpbrowne@uwaterloo.ca>
2023-09-09 12:00:02 +0000

    version 1.1

--- src/leptokurticMixture.c
+++ src/leptokurticMixture.c
@@ -1,58 +1,67 @@
-// See section 6.6.1 of Writing R Extensions (Fortran character strings)
-#define USE_FC_LEN_T
 
-#include <R_ext/Lapack.h>
-#include <stdbool.h>
+
+#ifndef  USE_FC_LEN_T
+# define USE_FC_LEN_T
+#endif
+#include <Rconfig.h>
+#include <R_ext/BLAS.h>
+#ifndef FCONE
+# define FCONE
+#endif
+
+#ifndef ILP64
+# define La_INT int
+# define La_LGL int
+#endif
 
 #include <R.h>
+#include <Rdefines.h>
+#include <Rmath.h>
+
+#include <stdbool.h>
 #include <Rinternals.h>
-#include <stdlib.h> // for NULL
+#include <R_ext/Lapack.h>
 #include <R_ext/Rdynload.h>
 
-/* FIXME:
-   Check these declarations against the C/Fortran source code.
-*/
 
-/* .Call calls */
-extern SEXP riccatiCareSolution(SEXP);
+extern SEXP riccatiCareSolution(SEXP x);
 
 static const R_CallMethodDef CallEntries[] = {
     {"riccatiCareSolution", (DL_FUNC) &riccatiCareSolution, 1},
     {NULL, NULL, 0}
 };
 
-void R_init_leptokurticMixture(DllInfo *dll)
-{
+void R_init_leptokurticMixture(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
 
 
+//La_extern La_LGL F77_NAME(selector)(const double* real, const double* img );
 
-#ifndef FCONE
-# define FCONE
-#endif
-int sortbynegreal(const double* real, const double* img) {
+La_LGL sortbynegreal(const double* real, const double* img) {
   if (*real < 0  )
     return(true);
   else
     return(false);
 }
 
-SEXP riccatiCareSolution(SEXP x) {
+
+
+extern SEXP riccatiCareSolution(SEXP x) {
   int *dim, n, m, info, izero = 0, lwork = -1;
   double *work, tmp;
   
   dim = INTEGER(getAttrib(x, R_DimSymbol));
-  
+
   n = dim[0];
   int bwork[n];
   m = n/2;
   
-  dgees_("V", "S", sortbynegreal, dim, (double *) NULL, dim, &izero,
-         (double *) NULL, (double *) NULL, (double *) NULL, dim,
-         &tmp, &lwork, bwork, &info FCONE FCONE);
-  
+  F77_CALL(dgees)("V", "S", &sortbynegreal, dim, (double *) NULL, dim, &izero,
+            (double *) NULL, (double *) NULL, (double *) NULL, dim,
+            &tmp, &lwork, bwork, &info FCONE FCONE);
+    
   lwork = (int) tmp;
   work = (double*)malloc( lwork*sizeof(double) );
   
@@ -60,10 +69,9 @@ SEXP riccatiCareSolution(SEXP x) {
   wr = (double*)malloc( n*sizeof(double) );
   wi = (double*)malloc( n*sizeof(double) );
   matz = (double*)malloc( n*n*sizeof(double) );
-  
-  dgees_("V", "S", sortbynegreal, dim,  REAL(x), dim, &izero, wr, wi,
-         matz, dim, work, &lwork, bwork, &info FCONE FCONE);
-  
+
+  F77_CALL(dgees)("V", "S", &sortbynegreal, dim,  REAL(x), dim, &izero, wr, wi,
+            matz, dim, work, &lwork, bwork, &info FCONE FCONE);
   SEXP ans = PROTECT(allocMatrix(REALSXP, m, m));
   
   double *px = REAL(ans), *py;
@@ -80,12 +88,14 @@ SEXP riccatiCareSolution(SEXP x) {
   ipiv = (int*)malloc( m*sizeof(int) );
   int num = m, nrhs = m, lda = m, ldb = m, info2;
   
-  dgesv_( &num, &nrhs, py, &lda, ipiv, px, &ldb, &info2);
-  
+  F77_CALL(dgesv)( &num, &nrhs, py, &lda, ipiv, px, &ldb, &info2 );
+    
   free(matz); free(wr); free(wi); free(ipiv);  free(work);
   
   UNPROTECT(1);
   return ans;
 }
+
+
 
 
